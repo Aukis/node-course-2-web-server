a555d6b61486ccb2038a598a0ff7f4609441770e
aukis <auge333@gmail.com>
2016-11-26 17:57:01 +0200

    add project page

--- server.js
+++ server.js
@@ -48,6 +48,13 @@ app.get('/about', (req,res)=> {
   });
 });
 
+app.get('/project', (req,res)=> {
+  res.render('project.hbs', {
+    pageTitle: 'Projects page'
+  });
+});
+
+
 app.get('/bad', (req,res)=> {
   res.send({
     errorMessage: 'Unable to handle request'
